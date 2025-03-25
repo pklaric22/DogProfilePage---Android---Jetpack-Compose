8265c786e2b2391030aab914c044e8c9886ac3c7
pklaric22 <pklaric22@student.foi.hr>
2025-03-25 18:53:01 +0100

    Added card composable and verticalScroll

--- app/src/main/java/com/example/dogprofilepage/ProflePage.kt
+++ app/src/main/java/com/example/dogprofilepage/ProflePage.kt
@@ -11,8 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,54 +29,64 @@ import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 @Composable
 fun ProfilePage() {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    )
-    {
-        Image(
-            painter = painterResource(id = R.drawable.dog),
-            contentDescription = "Slika psa",
-            modifier = Modifier
-                .size(100.dp)
-                .clip(CircleShape)
-                .border(
-                    width = 2.dp,
-                    color = androidx.compose.ui.graphics.Color.Red,
-                    shape = CircleShape
-                ),
-            contentScale = ContentScale.Crop
+    Card (
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 6.dp),
+        modifier = Modifier.fillMaxSize().
+            padding(top = 100.dp, bottom = 100.dp, start = 16.dp, end = 16.dp)
+            .border(width = 2.dp, color = androidx.compose.ui.graphics.Color.White, shape = RoundedCornerShape(30.dp))
+    ){
+        //Content of our card including dog image, followers, Description etc
+        Column(
+            Modifier.verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         )
+        {
+            Image(
+                painter = painterResource(id = R.drawable.dog),
+                contentDescription = "Slika psa",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+                    .border(
+                        width = 2.dp,
+                        color = androidx.compose.ui.graphics.Color.Red,
+                        shape = CircleShape
+                    ),
+                contentScale = ContentScale.Crop
+            )
 
-        Text(text = "Belgijski ovcar")
-        Text(text = "Belgija")
+            Text(text = "Belgijski ovcar")
+            Text(text = "Belgija")
 
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxWidth().padding(16.dp)
-        ) {
-            ProfileStats("150", "Pratitelja")
-            ProfileStats("100", "Prati")
-            ProfileStats("30", "Objava")
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                ProfileStats("150", "Pratitelja")
+                ProfileStats("100", "Prati")
+                ProfileStats("30", "Objava")
 
-        }
-
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier.fillMaxWidth().padding(16.dp)
-        ) {
-            Button(onClick = { /*TODO*/ } ){
-                Text(text = "Prati korisnika")
             }
 
-            Button(onClick = {/*TODO*/}) {
-                Text(text = "Posalji poruku")
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                Button(onClick = { /*TODO*/ } ){
+                    Text(text = "Prati korisnika")
+                }
+
+                Button(onClick = {/*TODO*/}) {
+                    Text(text = "Posalji poruku")
+                }
+
             }
 
         }
-
     }
 }
 
